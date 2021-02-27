e902586c6166bc69e0690f0661c209689ec7aa65
Steren <steren@google.com>
2021-02-27 21:52:14 +0000

    print results

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ function bindListeners() {
   }
 };
 
+function printResults(results) {
+  console.log(results);
+  const list = document.getElementById('results');
+  const template = document.getElementById('result-row');
+
+  // clean the list
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+
+  // Print top regions
+  for(let i = 0; i < 5; i++) {
+    let row = template.content.cloneNode(true);
+    row.querySelector('.region').textContent = results[i].region;
+    list.appendChild(row);
+  }
+
+}
+
 function recommendRegion() {
   let params = {
     weights: {}
@@ -16,8 +35,7 @@ function recommendRegion() {
   for(const input of inputs) {
     params.weights[input.name] = parseInt(input.value, 10) / 100;
   }
-	regionOptimizer(params)
-  .then(results => console.log(results));
+	regionOptimizer(params).then(printResults);
 };
 
 bindListeners();
